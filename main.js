e4dddc59a8f861379d8741f00dd15ae7035404f8
manolo-s <manolo.schotsborg@outlook.com>
2016-08-19 18:05:01 +0100

    add startpage message

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
-var months = {0: "January", 1: "February", 2: "March", 3: "April", 
+var months = {0: "January", 1: "February", 2: "March", 3: "April",
 			  4: "May", 5: "June", 6: "July", 7: "August", 8: "September",
 			  9: "October", 10: "November", 11: "December"};
 
@@ -30,9 +30,8 @@ function timestampService(req, res){
 
 app.get('/:id', timestampService);
 
+app.get('/', function (req, res) {
+	res.send('<h1>Timestamp Microservice</h1>');
+});
+
 app.listen(port);
-
-console.log('Server running at http://127.0.0.1:' + port + '/');
-
-
-
